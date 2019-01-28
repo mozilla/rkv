0aae0fee0e7af5f10440db773e00e5ac6957842c
Myk Melez <myk@mykzilla.org>
2019-01-28 13:48:09 -0800

    refactor read_transform into parent module

--- src/store/multi.rs
+++ src/store/multi.rs
@@ -8,8 +8,11 @@
 // CONDITIONS OF ANY KIND, either express or implied. See the License for the
 // specific language governing permissions and limitations under the License.
 
-use lmdb;
-
+use crate::{
+    error::StoreError,
+    store::read_transform,
+    value::Value,
+};
 use lmdb::{
     Cursor,
     Database,
@@ -20,29 +23,6 @@ use lmdb::{
     Transaction,
     WriteFlags,
 };
-
-use crate::error::StoreError;
-
-use crate::value::{
-    OwnedValue,
-    Value,
-};
-
-fn read_transform(val: Result<&[u8], lmdb::Error>) -> Result<Option<Value>, StoreError> {
-    match val {
-        Ok(bytes) => Value::from_tagged_slice(bytes).map(Some).map_err(StoreError::DataError),
-        Err(lmdb::Error::NotFound) => Ok(None),
-        Err(e) => Err(StoreError::LmdbError(e)),
-    }
-}
-
-fn read_transform_owned(val: Result<&[u8], lmdb::Error>) -> Result<Option<OwnedValue>, StoreError> {
-    match val {
-        Ok(bytes) => Value::from_tagged_slice(bytes).map(|v| Some(OwnedValue::from(&v))).map_err(StoreError::DataError),
-        Err(lmdb::Error::NotFound) => Ok(None),
-        Err(e) => Err(StoreError::LmdbError(e)),
-    }
-}
 
 #[derive(Copy, Clone)]
 pub struct MultiStore {
@@ -129,6 +109,14 @@ impl MultiStore {
 }
 
 /*
+fn read_transform_owned(val: Result<&[u8], lmdb::Error>) -> Result<Option<OwnedValue>, StoreError> {
+    match val {
+        Ok(bytes) => Value::from_tagged_slice(bytes).map(|v| Some(OwnedValue::from(&v))).map_err(StoreError::DataError),
+        Err(lmdb::Error::NotFound) => Ok(None),
+        Err(e) => Err(StoreError::LmdbError(e)),
+    }
+}
+
 impl<'env> Iterator for MultiIter<'env> {
     type Item = Iter<'env>;
 
