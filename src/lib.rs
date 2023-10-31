f20bd809eda7696c092ea47cc39e3aa68f9188c5
Kagami Sascha Rosylight <saschanaz@outlook.com>
2023-10-31 11:43:45 +0100

    Remove lmdb feature

--- src/lib.rs
+++ src/lib.rs
@@ -209,8 +209,6 @@ mod manager;
 mod readwrite;
 
 pub mod backend;
-#[cfg(feature = "lmdb")]
-pub mod migrator;
 pub mod store;
 pub mod value;
 
@@ -218,8 +216,6 @@ pub use backend::{DatabaseFlags, EnvironmentFlags, WriteFlags};
 pub use env::Rkv;
 pub use error::{DataError, MigrateError, StoreError};
 pub use manager::Manager;
-#[cfg(feature = "lmdb")]
-pub use migrator::Migrator;
 pub use readwrite::{Readable, Reader, Writer};
 pub use store::{keys::EncodableKey, single::SingleStore, CloseOptions, Options as StoreOptions};
 pub use value::{OwnedValue, Value};
